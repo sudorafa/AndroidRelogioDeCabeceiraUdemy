04af06fd0ae74ba56e03360bbf52ab634345c160
Rafa Lima <paracafa@gmail.com>
2017-12-22 12:16:01 -0300

    Aula 37 - Criado thread para rodar a hora ao vivo a cada milisegundo; No ciclo de vida: onResume chama método para rodar hora, onStop declara váriavel para false, pois no método verifica se tá falso para não continuar.

--- app/src/main/java/com/example/orafa/androidrelogiodecabeceiraudemy/MainActivity.java
+++ app/src/main/java/com/example/orafa/androidrelogiodecabeceiraudemy/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.orafa.androidrelogiodecabeceiraudemy;
 
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -11,12 +15,16 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Handler mHandler = new Handler();
+    private Runnable mRunnable;
+    private boolean mRunnableStopped = false;
+
     @BindView(R.id.text_view_hour_minute)
     TextView mTextViewHourMinute;
     @BindView(R.id.text_view_seconds)
     TextView mTextViewSeconds;
     @BindView(R.id.check_box_battery)
-    CheckBox mCheckBoxBatery;
+    CheckBox mCheckBoxBattery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +32,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.mRunnableStopped = false;
+        this.startBedside();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        this.mRunnableStopped = true;
+    }
+
+    private void startBedside() {
+
+        final Calendar calendar = Calendar.getInstance();
+        this.mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                if (mRunnableStopped) {
+                    return;
+                }
+                calendar.setTimeInMillis(System.currentTimeMillis());
+
+                String hourMinutesFormat = String.format("%02d:%02d", calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE));
+                String secondFormat = String.format("%02d", calendar.get(Calendar.SECOND));
+
+                mTextViewHourMinute.setText(hourMinutesFormat);
+                mTextViewSeconds.setText(secondFormat);
+
+                //mile de agora
+                long now = SystemClock.uptimeMillis();
+                //calcular para cair o próximo segundo
+                long next = now + (1000 - (now % 1000));
+
+                //rodar conforme chamado | calculo acima
+                mHandler.postAtTime(mRunnable, next);
+            }
+        };
+        this.mRunnable.run();
     }
 }
